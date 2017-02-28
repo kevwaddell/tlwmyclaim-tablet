41c2dd71f41b316e63ddc665a60009a02fd41deb
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2017-02-28 11:36:18 +0000

    Update jumbotron on general page template

--- page.php
+++ page.php
@@ -3,11 +3,11 @@
 <?php if ( have_posts() ) : ?>
 
 	<?php while ( have_posts() ) : the_post(); ?>
-	<div class="jumbotron wht-border-bottom">
+	<div class="jumbotron wht-border-bottom text-center">
 		<div class="container-fluid">
 			<div class="row">
 				<div class="col-md-10 col-md-offset-1">
-					<h1 class="text-center"><?php the_title(); ?></h1>	
+					<h1><?php the_title(); ?></h1>	
 				</div>
 			</div>
 		</div>
