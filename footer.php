2767eafec7195b70b772e6cc79060d40477761c7
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2017-02-28 14:28:47 +0000

    Hide some things from admin and change footer styles.

--- footer.php
+++ footer.php
@@ -1,13 +1,17 @@
+	<?php if ( !current_user_can( 'administrator' ) ) { ?>
 	<?php get_template_part( 'parts/global/freephone', 'number' ); ?>
+	<?php } ?>
 	
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="container-fluid">
+			<?php if ( !current_user_can( 'administrator' ) ) { ?>
 			<div class="col-xs-12">
 				<?php wp_nav_menu(array( 'container_class' => 'footer-links', 'theme_location' => 'footer-menu', 'fallback_cb' => false ) ); ?>
 			</div>
+			<?php } ?>
 			<div class="col-xs-6 col-md-4 col-md-offset-2">
 				<div id="tlw-footer-logo" target="_blank"><span class="sr-only">TLW Solicitors</span></div>
-				<div class="copyright-legal text-center">&copy; 2016 TLW Solicitors. All rights reserved.<br>Authorised and regulated by the <a href="http://www.sra.org.uk/home/home.page" target="_blank">Solicitors Regulation Authority</a></div>
+				<div class="copyright-legal text-center">&copy; TLW Solicitors <?php echo date('Y'); ?>. All rights reserved.<br>Authorised and regulated by the <a href="http://www.sra.org.uk/home/home.page" target="_blank">Solicitors Regulation Authority</a></div>
 			</div>
 			<div class="col-xs-6 col-md-4">
 				<a href="https://www.tlwsolicitors.co.uk" class="visit-site-btn btn btn-block btn-lg" target="_blank">Visit the <span>TLW Solicitors</span> website</a>
--- page.php
+++ page.php
@@ -16,7 +16,9 @@
 		<div class="container-fluid">
 			<div class="row">
 				<div class="col-md-10 col-md-offset-1">
-					<?php the_content(); ?>	
+					<div class="entry">
+						<?php the_content(); ?>	
+					</div>
 				</div>
 			</div>
 		</div>						
