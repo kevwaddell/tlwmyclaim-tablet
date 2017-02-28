818bd4b55ba6c14ff94929d762186748f52d9d18
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2017-02-28 11:25:25 +0000

    Move jumbotron on login page

--- page-template/user-login-page.php
+++ page-template/user-login-page.php
@@ -9,20 +9,20 @@ Template Name: User Login Forms
 <?php if ( have_posts() ) : ?>
 
 		<?php while ( have_posts() ) : the_post(); ?>
-
+						
+		<div class="jumbotron wht-border-bottom">
+			<div class="container-fluid">
+			<?php the_content(); ?>	
+			</div>
+		</div>
+		
 		<article id="login-form" <?php post_class(); ?>>
-				
-				<div class="jumbotron wht-border-bottom">
-					<div class="container-fluid">
-					<?php the_content(); ?>	
-					</div>
+
+			<section class="user-login-forms">
+				<div class="container-fluid">
+					<?php echo do_shortcode( "[theme-my-login]" ) ?>		
 				</div>
-				
-				<section class="user-login-forms">
-					<div class="container-fluid">
-						<?php echo do_shortcode( "[theme-my-login]" ) ?>		
-					</div>
-				</section>
+			</section>
 				
 		</article><!-- #post-## -->
 
