64bd4c1c1c42cd4d58b0e3e2f4b9402ca71f0392
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2017-02-28 12:11:13 +0000

    Update referer list page

--- page-template/referrers-list-page.php
+++ page-template/referrers-list-page.php
@@ -23,88 +23,88 @@ $users = get_users( $users_args );
 <?php if ( have_posts() ) : ?>
 
 		<?php while ( have_posts() ) : the_post(); ?>
-
-		<article <?php post_class(); ?>>
-			
-			<div class="jumbotron wht-border-bottom">
-				<div class="container-fluid">
-				<?php the_content(); ?>	
-				</div>
-			</div>
-			
-			<?php if (!empty($users)) { ?>
-			<section id="users-list">
-			<div class="container-fluid">	
-				<div class="panel panel-default">	
-		
-					<div class="panel-heading text-center">Referrers</div>	
-		
-					<table class="table table-bordered">
-						<tbody>
-							<tr>
-								<th width="35%" class="text-center">Company</th>
-								<th width="35%" class="text-center">Main contact</th>
-								<th width="25%" class="text-center">Number of cases</th>
-						  	</tr>
-						  	
-						  	<?php foreach ($users as $user) { 
-							$company = get_user_meta($user->ID, 'company_name', true);
-							$user_data = get_userdata($user->ID);
-							//echo '<pre>';print_r($user_data);echo '</pre>';
-							
-							$claims_args = array(
-							'posts_per_page' => -1,
-							'post_type'		=> 'post',
-							'post_status'	=>	'private',
-							'meta_key'	=> 'src_ref',
-							'meta_value'	=> $user_data->user_nicename
-							);
-							$claims = get_posts( $claims_args );
-							$open = 0;
-							$closed = 0;
-							//echo '<pre class="debug">';print_r($claims);echo '</pre>';
-						  	?>
-						  	<tr>
-								<td class="text-center" style="vertical-align: middle;"><?php echo $company; ?></td>
-								<td class="text-center" style="vertical-align: middle;"><?php echo $user_data->display_name; ?></td>
-								<td class="text-center" style="vertical-align: middle;">
-								  <?php foreach ($claims as $claim) { 
-								   $case_status = get_post_meta( $claim->ID, 'case_status', true);
-								   		if ($case_status == 'open') {
-									   	$open++;	
-								   		}
-								   		
-								   		if ($case_status == 'closed') {
-									   	$closed++;	
-								   		}
-								   }
-								  ?>
-								 <span class="label block label-success" style="margin-bottom: 5px;">Open cases: <?php echo $open; ?></span> 
-								 <span class="label block label-danger">Closed cases: <?php echo $closed; ?></span>
-									</div>
-								</td>
-						  	</tr>
-						  	<?php } ?>
-						  	
-						</tbody>
-					</table>
 					
-				</div>
-			</div>
-			</section>
-			<?php } else { ?>
+		<div class="jumbotron wht-border-bottom">
 			<div class="container-fluid">
+			<?php the_content(); ?>	
+			</div>
+		</div>
+		
+		<article <?php post_class(); ?>>
+			<div class="container-fluid">	
 				<div class="row">
-					<div class="col-xs-8 col-xs-offset-2">
+					<div class="col-md-10 col-md-offset-1">
+						<?php if (!empty($users)) { ?>
+						<section id="users-list">
+								
+								<div class="panel panel-default">	
+					
+								<div class="panel-heading text-center">Referrers</div>	
+					
+								<table class="table table-bordered">
+									<tbody>
+										<tr>
+											<th width="34%" class="text-center">Company</th>
+											<th width="33%" class="text-center">Main contact</th>
+											<th width="33%" class="text-center">Number of cases</th>
+									  	</tr>
+									  	
+									  	<?php foreach ($users as $user) { 
+										$company = get_user_meta($user->ID, 'company_name', true);
+										$user_data = get_userdata($user->ID);
+										//echo '<pre>';print_r($user_data);echo '</pre>';
+										
+										$claims_args = array(
+										'posts_per_page' => -1,
+										'post_type'		=> 'post',
+										'post_status'	=>	'private',
+										'meta_key'	=> 'src_ref',
+										'meta_value'	=> $user_data->user_nicename
+										);
+										$claims = get_posts( $claims_args );
+										$open = 0;
+										$closed = 0;
+										//echo '<pre class="debug">';print_r($claims);echo '</pre>';
+									  	?>
+									  	<tr>
+											<td class="text-center" style="vertical-align: middle;"><?php echo $company; ?></td>
+											<td class="text-center" style="vertical-align: middle;"><?php echo $user_data->display_name; ?></td>
+											<td class="text-center" style="vertical-align: middle;">
+											  <?php foreach ($claims as $claim) { 
+											   $case_status = get_post_meta( $claim->ID, 'case_status', true);
+											   		if ($case_status == 'open') {
+												   	$open++;	
+											   		}
+											   		
+											   		if ($case_status == 'closed') {
+												   	$closed++;	
+											   		}
+											   }
+											  ?>
+											 <span class="label block label-success" style="margin-bottom: 5px;">Open cases: <?php echo $open; ?></span> 
+											 <span class="label block label-danger">Closed cases: <?php echo $closed; ?></span>
+												</div>
+											</td>
+									  	</tr>
+									  	<?php } ?>
+									  	
+									</tbody>
+								</table>
+								
+							</div>
+							
+						</section>
+						<?php } else { ?>
+			
 						<div class="well well-lg well-message text-center">
 							<h2>Sorry</h2>
 							<p>There are no referrers at the moment.</p>
 						</div>
+				
+						<?php } ?>
 					</div>
 				</div>
 			</div>
-			<?php } ?>
-
 		</article><!-- #post-## -->
 
 		<?php endwhile; ?>
